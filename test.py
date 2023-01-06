0cb1f34c36993ebd7284cba20b4473326e84a7dc
Tosin Ayodele <devtosxn@gmail.com>
2023-01-06 06:55:57 +0100

    Add tests for account creation

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import unittest
 from user import User
+from db import WafiDB
 
 class Test(unittest.TestCase):
+    def test_user_can_be_created(self):
+        user = User("johndoe@gmail.com")
+        db = WafiDB()
+        db.add_user(user)
+        self.assertEqual(user.email, "johndoe@gmail.com")
+        self.assertEqual(len(db.users), 1)
+
     def test_user_can_retrieve_balance(self):
         user = User("john@gmail.com")
         self.assertEqual(user.balance, 0)
